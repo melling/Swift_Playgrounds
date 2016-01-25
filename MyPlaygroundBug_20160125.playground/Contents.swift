7c23d5413b9d11610536dcc7e0aed9f8c4bc8cdc
Michael Mellinger <mmellinger66@gmail.com>
2016-01-25 18:19:43 -0500

    Added playground bug

---
+++ MyPlaygroundBug_20160125.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//: Color from Pattern
+
+let image = UIImage(named:"texture.png")
+// Uncomment this line and Xcode will crash
+//let colorPattern = UIColor(patternImage: image!)
